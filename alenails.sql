7792f22bc4dd5842f7afc767d1436804db2c2e69
SrPitaya <axelmis71@gmail.com>
2025-04-10 14:38:43 -0600

    Second commit, add sql to community xd

---
+++ alenails.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-06-2024 a las 00:01:23
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `alenails`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administradora`
+--
+
+CREATE TABLE `administradora` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `administradora`
+--
+
+INSERT INTO `administradora` (`id`, `user_name`, `password`) VALUES
+(1, 'ale@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `idclientes` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apodo` varchar(100) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`idclientes`, `nombre`, `apodo`, `edad`, `email`, `password`) VALUES
+(1, 'Axel', 'Pitaya', 20, 'axel@gmail.com', '123'),
+(16, 'Francisco', 'Panchito', 48, 'fra@gmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `inicio` datetime DEFAULT NULL,
+  `fin` datetime DEFAULT NULL,
+  `colortexto` varchar(7) DEFAULT NULL,
+  `colorfondo` varchar(7) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `eventos`
+--
+
+INSERT INTO `eventos` (`id`, `titulo`, `descripcion`, `inicio`, `fin`, `colortexto`, `colorfondo`, `nombre`) VALUES
+(83, 'Manicure Básico', 'Calle 33A x68 y 70 #307, Residencial del bosque', '2024-06-08 13:37:00', '2024-06-08 14:37:00', '#f0dbdb', '#3788d8', 'Axel Isai Mis Aguilar'),
+(99, 'Manicure Básico', 'Temozon', '2024-06-14 16:53:00', '2024-06-14 17:53:00', '#ffffff', '#3788d8', 'Francisco Solano Mis Chan'),
+(102, 'Manicure de Lujo', 'calle conocida', '2024-06-27 21:59:00', '2024-06-27 22:59:00', '#ed1b21', '#51f423', 'Francisco Solano Mis Chan');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventospredefinidos`
+--
+
+CREATE TABLE `eventospredefinidos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  `horainicio` time DEFAULT NULL,
+  `horafin` time DEFAULT NULL,
+  `colortexto` varchar(7) DEFAULT NULL,
+  `colorfondo` varchar(7) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `eventospredefinidos`
+--
+
+INSERT INTO `eventospredefinidos` (`id`, `titulo`, `horainicio`, `horafin`, `colortexto`, `colorfondo`) VALUES
+(42, 'Descanso', '00:00:00', '23:59:00', '#ffffff', '#000000');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administradora`
+--
+ALTER TABLE `administradora`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`idclientes`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `eventospredefinidos`
+--
+ALTER TABLE `eventospredefinidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administradora`
+--
+ALTER TABLE `administradora`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `idclientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+
+--
+-- AUTO_INCREMENT de la tabla `eventospredefinidos`
+--
+ALTER TABLE `eventospredefinidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
