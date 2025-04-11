758baa4f23bcbe5ee1e3c7ce45ca9f40e5512d69
SrPitaya <axelmis71@gmail.com>
2025-04-11 16:17:14 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@ function regresarConexion(){
   $server = "localhost";
   $usuario = "root";
   $clave = "";
-  $base = "alenails";
+  $base = "salonbelleza";
 
   $conexion = mysqli_connect($server, $usuario, $clave, $base) or die ("Problemas con la Conexion");
   mysqli_set_charset($conexion, 'utf8');
